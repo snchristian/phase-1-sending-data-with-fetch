fedc8f8e7f5a004bedf1a2c7f303b08f5be58f25
Shantel <snchristian8@gmail.com>
2021-08-25 12:53:58 -0400

    commit tree 1e69cf8bb809feeed2f443458f1283c67553e8ef

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Add your code here
+const personData = {
+    name:"Steve",
+    email:"steve@steve.com"
+}
+
+function submitData(){
+    return fetch('http://localhost:3000/users',{
+        method: 'POST',
+        headers:{
+            "Content-Type":"application/json",
+            Accept: "application/json",
+        },
+        body: JSON.stringify(personData),
+
+    }) 
+    .then(function(response){
+        return response.json()
+    })
+    .then(function(object){
+        document.querySelector('body').innerHTML= object.id
+    })
+    .catch(function(error){
+        alert("Bad Thing please check your code!");
+        document.querySelector('body').innerHTML= error.message
+    })
+}
+ 
